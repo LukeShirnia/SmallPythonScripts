06380b1574d3925369ac02d95ce56e92f3001ee0
Luke Shirnia <lukeshirnia@btinternet.com>
2017-03-15 08:25:08 +0000

    Update os_check.py

--- os_check.py
+++ os_check.py
@@ -1,12 +1,24 @@
 #!/usr/bin/python
 import platform
 import sys
-# does not currently work as expected on RHEL/CENTOS 5
+# does not currently work on RHEL/CENTOS 5
 
-supported_centos = [6, 7]
+supported_centos = ['5', '6', '7']
 supported_ubuntu = [12, 14, 16]
 CentOS_RedHat_Distro = ['redhat', 'centos', 'red', 'red hat']
 Ubuntu_Debian_Distro = ['ubuntu', 'debian']
+
+def version_check():
+	os_version = platform.linux_distribution()[1]
+	os_version = os_version.split(".")[0]
+	
+	if os_version in supported_centos:
+		print platform.linux_distribution()[1]
+	elif os_version in supported_ubuntu:
+		print platform.linux_distribution()[1]
+	else:
+		print "Something wrong"
+		print os_version 
 
 def os_check():
         os_platform = platform.system()
@@ -15,13 +27,14 @@ def os_check():
                 distro = distro.split()[0]
                 return distro
         else:
-                print "Stop Using a Rubbish OS!!"
-                return "Error"
+		print "Stop Using a Rubbish OS!!"
 
 os_check_value = os_check()
 if os_check_value.lower() in CentOS_RedHat_Distro:
-        print "RedHat"
+	print os_check_value
+	version_check()
 elif os_check_value.lower() in Ubuntu_Debian_Distro:
-        print "Ubuntu"
+	print os_check_value
+	version_check()
 else:
-        print "OS Not Recognised"
+	print "OS Not Recognised"
