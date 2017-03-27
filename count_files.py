0f087aedd00880a3bca1aa75fb6c2f195f260a1e
Luke Shirnia <lukeshirnia@btinternet.com>
2017-03-27 07:22:13 +0100

    Create count_files.py

---
+++ count_files.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+from sys import argv
+import os
+
+def get_files_count(paths):
+	files = folders = 0
+	for _, dirnames, filenames in os.walk(paths):
+       	  # ^ this idiom means "we won't be using this value"
+		files += len(filenames)
+		folders += len(dirnames)
+	print paths
+	print "{:,} files, {:,} folders".format(files, folders)
+
+if len(argv) == 1:
+	print "Please enter a path to count"
+if len(argv) == 2:
+	script, path1 = argv 
+	paths = path1
+	get_files_count(paths)
+if len(argv) >= 3:
+	print "Too many arguments"
