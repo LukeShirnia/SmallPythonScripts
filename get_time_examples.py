d6fbf111f8975db2bd2ddeb46831aaa8b170bd29
Luke Shirnia <lukeshirnia@btinternet.com>
2017-04-05 07:05:15 +0100

    Update get_time_examples.py

--- get_time_examples.py
+++ get_time_examples.py
@@ -10,7 +10,7 @@ now_mins_10 = now_mins_10.strftime('%b %d %H:%M')
 now_mins_80 = now - datetime.timedelta(minutes = 80)
 now_mins_80 = now_mins_80.strftime('%b %d %H:%M')
 now_mins_1440 = now - datetime.timedelta(minutes = 1440)
-now_mins_1440 = now_mins_1440.strftime('%b %d %H:%M')
+now_mins_1440 = now_mins_1440.strftime('%b  %d %H:%M')
 
 print "Current time            %s" % (now_time)
 print "Minus 10:               %s" % (now_mins_10)
